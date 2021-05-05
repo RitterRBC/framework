9aafc628dae412ca553414681157a136149d5ccc
Teilnehmer <thorsten.daempfert@gmail.com>
2021-05-05 14:41:20 +0200

    2021-05-05 Working build for c64 net462

--- Tools/open-samples-csprojs.bat
+++ Tools/open-samples-csprojs.bat
@@ -1,12 +1,12 @@
 @echo off
 
-set samples=C:\Projects\Accord.NET\framework\Samples
-set notepad="C:\Program Files (x86)\Notepad++\notepad++.exe"
+set samples=Z:\GitHub\framework\Samples
+set notepad="C:\Program Files\Microsoft VS Code\Code.exe"
 echo.
 
 echo %samples%
 for /r "%samples%" %%F in (*.csproj) do (
   echo %%F
-  %notepad% %%F
+  %notepad% "%%F"
 )
 
